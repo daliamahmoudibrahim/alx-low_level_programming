85cee9166a3fa3f4925a5fbadb7e0b393f7b273b
Dalia Mahmoud <dalia.mahmoud.ibrahim1@gmail.com>
2023-03-16 12:04:43 -0700

    add files in c helloworld

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - output statment
  *
- * Return: Always (0);
+ * Return: Always 0;
  */
 int main(void) 
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
