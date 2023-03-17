d8839a276fa1e2e9b51ea4e0f43c8aaf78fdc8c3
Dalia Mahmoud <dalia.mahmoud.ibrahim1@gmail.com>
2023-03-17 03:55:31 -0700

    second day to learn c if for while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,8 @@
 /**
  * main - type the number zero or positive or negative
  *
- * Return : 0
+ * Return : Always (0);
+ *
  */
 /* betty style doc for function main goes there */
 int main(void)
@@ -14,7 +15,7 @@ int main(void)
 	     srand(time(0));
 		n = rand() - RAND_MAX / 2;
 
-		if (n >= 0)
+		if (n > 0)
 			printf("%d is positive\n", n);
 		else if (n == 0)
 			printf("%d is zero\n", n);
