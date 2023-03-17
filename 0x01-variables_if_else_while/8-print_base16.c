0aaeddb3930ed26dc73a78914eed1b9fc59e7cdb
Dalia Mahmoud <dalia.mahmoud.ibrahim1@gmail.com>
2023-03-17 08:28:30 -0700

    add file print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - print all hexdecimal number base 16
+*
+* Return: Always 0;
+*/
+int main(void)
+{
+	int small;
+	int hex;
+
+	for (small = 0; small < 9; small++)
+		putchar('0' + small);
+	for (hex = 97; hex < 103; hex++)
+		putchar(hex);
+	putchar('\n');
+	return (0);
+}
