7142eaf2ed974d1e6537183fd1f0c82bf324ff4b
Dalia Mahmoud <dalia.mahmoud.ibrahim1@gmail.com>
2023-03-17 10:19:43 -0700

    add file 100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - print group of numbers contine 2 numbers
+*
+* Return: Always 0;
+*/
+int main(void)
+{
+	int i;
+	int j;
+	int ch = ',';
+	int space = ' ';
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i == 8 && j == 9)
+			{
+				continue;
+			}
+			putchar(ch);
+			putchar(space);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
