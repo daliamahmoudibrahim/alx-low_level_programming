fbe565597d5277d1420933eda0992d87427f9367
Dalia Mahmoud <dalia.mahmoud.ibrahim1@gmail.com>
2023-03-17 07:58:01 -0700

    edit file 6-print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,10 +6,10 @@
 */
 int main(void)
 {
-	char num = '0';
+	int num = 0;
 
-	for (num = '0'; num <= '9'; num++)
-		putchar(num);
+	for (num = 0; num <= 9; num++)
+		putchar('0' + num);
 	putchar('\n');
 	return (0);
 }
