b4912774e1fbea0e53addf31e22ec21c488b6332
Dalia Mahmoud <dalia.mahmoud.ibrahim1@gmail.com>
2023-03-21 12:36:22 -0700

    add file 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+void print_alphabet()
+{
+	int ch = 97;
+	while (ch != 123)
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+
+}
