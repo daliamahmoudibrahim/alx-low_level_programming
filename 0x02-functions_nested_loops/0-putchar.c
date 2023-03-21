79a59df433acb0e8190c9ef9347a3381df9b9696
Dalia Mahmoud <dalia.mahmoud.ibrahim1@gmail.com>
2023-03-21 12:18:09 -0700

    edit file 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
-* main : print word _putchar
+* main -  print word _putchar;
+*
 *
 * Return: (0)
 */
-char _putchar (char c);
-int main (void)
+int main(void)
 {
 	char *ch = "_putchar";
 
 	while (*ch)
 	{
-		_putchar( *ch );
+		_putchar(*ch);
 		ch++;
-		
 	}
 	_putchar('\n');
-	return 0;
+	return (0);
 }
