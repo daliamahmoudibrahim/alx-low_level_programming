043d60d4584cf3fa161e602c4d9917c18e229f07
Dalia Mahmoud <dalia.mahmoud.ibrahim1@gmail.com>
2023-03-21 14:53:11 -0700

    add filt 6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: value of negative number
+ *
+ * Return: value abs (n);
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
