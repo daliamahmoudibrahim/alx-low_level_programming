d098a62b50d4210f7df67c92efdba7d2961ef3ea
daliamahmoudibrahim <dalia.mahmoud.ibrahim1@gmail.com>
2023-06-09 12:42:11 -0700

    create file 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -
+ * @argc: arguments
+ * @argv[]:array to store the arguments
+ * Return : name file
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n",argv[0]);
+	return(0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Learn How to use arguments passed to your program
