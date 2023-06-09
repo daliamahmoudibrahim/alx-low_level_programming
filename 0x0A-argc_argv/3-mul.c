75f6561425969b01a8178acec9bb143f80819a82
daliamahmoudibrahim <dalia.mahmoud.ibrahim1@gmail.com>
2023-06-09 13:45:15 -0700

    add file 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,9 +9,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int mul = atoi(argv[1]) * atoi(argv[2]);
+	int result;
 
-	(void)argc;
-	printf("%d\n", mul);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	result = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", result);
 	return (0);
 }
