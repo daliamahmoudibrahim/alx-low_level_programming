df2837b181e35c3a765abb8b76d71726b65fc0a8
daliamahmoudibrahim <dalia.mahmoud.ibrahim1@gmail.com>
2023-06-09 12:46:48 -0700

    create file 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main -
+ * main - return any name to file
  * @argc: arguments
- * @argv[]:array to store the arguments
- * Return : name file
+ * @argv:array to store the arguments
+ * Return: name file
  */
 int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("%s\n",argv[0]);
-	return(0);
+	printf("%s\n", argv[0]);
+	return (0);
 }
