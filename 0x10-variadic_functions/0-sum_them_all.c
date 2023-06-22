30e994d74dfec53ac1dd4d33801e80627e8ec82c
daliamahmoudibrahim <dalia.mahmoud.ibrahim1@gmail.com>
2023-06-22 04:12:35 -0700

    learn variadic functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all- sum all arguments
+ * @n: number of arguments
+ * Return: 0 or sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int sum = 0;
+
+	va_start(args, n);
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		unsigned int i;
+
+		for (i = 0; i < n; i++)
+		{
+			int x = va_arg(args, int);
+
+			sum += x;
+		}
+		return (sum);
+	}
+}
