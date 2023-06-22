472171a83d13d833ef0dc8ceb1633cc67f02d58d
daliamahmoudibrahim <dalia.mahmoud.ibrahim1@gmail.com>
2023-06-22 08:32:01 -0700

    edit file variadic_function.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,7 @@
+#ifndef veriadic_function
+#define veriadic_function
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+#endif
