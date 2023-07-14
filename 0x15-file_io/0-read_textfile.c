f80ac4bb7ba3fa361bb27ba604b6e178f365b12d
daliamahmoudibrahim <dalia.mahmoud.ibrahim1@gmail.com>
2023-07-14 10:04:34 -0700

    add dir files i/o

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters) {
+    FILE *file;
+    char *buffer;
+    ssize_t read_count;
+    ssize_t write_count;
+
+    if (filename == NULL)
+    {
+	    return (0);
+    }
+
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        return 0;
+    }
+
+    buffer = malloc(sizeof(char) * (letters + 1));
+    if (buffer == NULL) {
+        fclose(file);
+        return 0;
+    }
+
+    read_count = fread(buffer, sizeof(char), letters, file);
+    if (read_count < 0) {
+        fclose(file);
+        free(buffer);
+        return 0;
+    }
+
+    write_count = write(STDOUT_FILENO, buffer, read_count);
+    if (write_count != read_count) {
+        fclose(file);
+        free(buffer);
+        return 0;
+    }
+
+    fclose(file);
+    free(buffer);
+    return read_count;
+}
